f7f9ee5730ad6fd1620acd9aa5a4cfb516ccd146
saadk6401 <saadk6401@gmail.com>
2022-02-05 12:31:37 +0500

    first commit

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,43 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  text!: string;
+  id!: string;
+  day!: string;
+  reminder: boolean = false;
+  subscription: Subscription;
+  showAddTask!: boolean;
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onTogggle().subscribe((value) => this.showAddTask = value);
+  }
+
+  ngOnInit(): void {
+
+  }
+  onSubmit() {
+    if (!this.text) {
+      alert('please add task!')
+      return;
+    }
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+
+    }
+    this.onAddTask.emit(newTask)
+    ///
+    this.text = '',
+      this.day = '',
+      this.reminder = false;
+  }
+}
